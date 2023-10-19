08b5dc31b14874606cd721790de4116c9806d48d
JBautistaBeck <juanbautistabeck@gmail.com>
2023-10-19 13:37:15 -0300

    Config Tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className=" bg-black">
+      <header className=" bg-black">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
