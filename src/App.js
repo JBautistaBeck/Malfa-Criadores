f7c0b70cc1b0dd9c3affff6e01e7c9bd2cf942f7
JBautistaBeck <juanbautistabeck@gmail.com>
2023-10-20 10:46:41 -0300

    home

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
+import Home from "./views/home";
 
 function App() {
   return (
     <div className=" bg-black">
-      <header className=" bg-black">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home></Home>
+
     </div>
   );
 }
---
+++ src/views/home.jsx
@@ -0,0 +1,12 @@
+
+function Home() {
+    return (
+      <div className=" bg-green-500 h-screen">
+        <h1>Home</h1>
+  
+      </div>
+    );
+  }
+  
+  export default Home;
+  
