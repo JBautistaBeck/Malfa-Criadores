a1a4f5ec6b831f2a2ca0de270904172ae1ac350d
JBautistaBeck <juanbautistabeck@gmail.com>
2023-10-20 11:07:32 -0300

    nav bar y about

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import NavBar from "./components/NavBar";
+import About from "./views/about";
 import Home from "./views/home";
 
 function App() {
   return (
     <div className=" bg-black">
+      <NavBar></NavBar>
       <Home></Home>
+      <About></About>
 
     </div>
   );
---
+++ src/views/about.jsx
@@ -0,0 +1,12 @@
+
+function About() {
+    return (
+      <div className=" bg-orange-500 h-screen">
+        <h1>About</h1>
+  
+      </div>
+    );
+  }
+  
+  export default About;
+  
