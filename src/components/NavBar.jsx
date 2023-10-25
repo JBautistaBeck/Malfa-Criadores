8821cb1d9c5608ec33329f46021e15f9ead54b67
JBautistaBeck <juanbautistabeck@gmail.com>
2023-10-25 17:24:25 -0300

    Prettier

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,80 +1,82 @@
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from "react";
 
 function NavBar() {
-    const [scrolling, setScrolling] = useState(false);
+  const [scrolling, setScrolling] = useState(false);
 
-    useEffect(() => {
-      window.addEventListener('scroll', handleScroll);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
 
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setScrolling(true);
+    } else {
+      setScrolling(false);
+    }
+  };
 
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setScrolling(true);
-      } else {
-        setScrolling(false);
+  const handleAboutClick = (id) => {
+    if (id === "about") {
+      const aboutSection = document.getElementById("about");
+      if (aboutSection) {
+        aboutSection.scrollIntoView({ behavior: "smooth" });
       }
-    };
+    } else if (id === "proyectos") {
+      const proyectosSection = document.getElementById("proyectos");
+      if (proyectosSection) {
+        proyectosSection.scrollIntoView({ behavior: "smooth" });
+      }
+    } else if (id === "skills") {
+      const skillsSection = document.getElementById("skills");
+      if (skillsSection) {
+        skillsSection.scrollIntoView({ behavior: "smooth" });
+      }
+    } else if (id === "home") {
+      const homeSection = document.getElementById("home");
+      if (homeSection) {
+        homeSection.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  };
 
+  return (
+    <div
+      className={` text-xs  sm:text-base fixed top-0 w-full p-2 pl-6 flex justify-between duration-500 ${
+        scrolling
+          ? "backdrop-blur-lg bg-white opacity-90"
+          : " backdrop-blur-lg "
+      }`}
+    >
+      <p>"CODIGO"</p>
 
-
-    const handleAboutClick = (id) => {
-        if(id === "about") {
-
-          const aboutSection = document.getElementById('about');
-          if (aboutSection) {
-          aboutSection.scrollIntoView({ behavior: 'smooth' });
-          }
-
-        } else if (id === "proyectos") {
-
-          const proyectosSection = document.getElementById('proyectos');
-          if (proyectosSection) {
-          proyectosSection.scrollIntoView({ behavior: 'smooth' });
-          }
-
-        }  else if (id === "skills") {
-
-          const skillsSection = document.getElementById('skills');
-          if (skillsSection) {
-          skillsSection.scrollIntoView({ behavior: 'smooth' });
-          }
-
-        }  else if (id === "home") {
-
-          const homeSection = document.getElementById('home');
-          if (homeSection) {
-          homeSection.scrollIntoView({ behavior: 'smooth' });
-          }
-
-        }
-    };
-
-
-    return (
-      <div className={` text-xs  sm:text-base fixed top-0 w-full p-2 pl-6 flex justify-between duration-500 ${ scrolling ? 'backdrop-blur-lg bg-white opacity-90' : ' backdrop-blur-lg '}`}>
-
-        <p>"CODIGO"</p>
-
-        <div className="">
-
-            <button onClick={() => handleAboutClick("about")} className=" text-black border-b border-black mt-2 ml-6 scroll-link hover:text-slate-700 hover:border-slate-700">Sobre mí</button>
-            <button onClick={() => handleAboutClick("proyectos")} className="text-black border-b border-black mt-4 sm:mt-2 ml-3 sm:ml-6 scroll-link hover:text-slate-700 hover:border-slate-700">Proyectos</button>
-            <button onClick={() => handleAboutClick("skills")} className="text-black border-b border-black mt-2 ml-3 sm:ml-6 mr-2 sm:mr-4 scroll-link hover:text-slate-700 hover:border-slate-700">Habilidades</button>
-
-        </div>
-
-        
-  
+      <div className="">
+        <button
+          onClick={() => handleAboutClick("about")}
+          className=" text-black border-b border-black mt-2 ml-6 scroll-link hover:text-slate-700 hover:border-slate-700"
+        >
+          Sobre mí
+        </button>
+        <button
+          onClick={() => handleAboutClick("proyectos")}
+          className="text-black border-b border-black mt-4 sm:mt-2 ml-3 sm:ml-6 scroll-link hover:text-slate-700 hover:border-slate-700"
+        >
+          Proyectos
+        </button>
+        <button
+          onClick={() => handleAboutClick("skills")}
+          className="text-black border-b border-black mt-2 ml-3 sm:ml-6 mr-2 sm:mr-4 scroll-link hover:text-slate-700 hover:border-slate-700"
+        >
+          Habilidades
+        </button>
       </div>
-    );
-  }
-  
-  export default NavBar;
+    </div>
+  );
+}
+
+export default NavBar;
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,18 +1,15 @@
-
 function Home() {
-    return (
-      <div className=" bg-green-500 h-screen">
-        <h1 className=" text-cyan-400 pt-10">Home</h1>
-        <div className=" bg-yellow-500 w-20 h-20 m-auto border-8 border-solid border-blue-700">
-          <p>Hola</p>
-        </div>
-        <div className=" bg-white w-20 h-20 m-auto">
-          <p>Chau</p>
-        </div>
-  
+  return (
+    <div className=" bg-green-500 h-screen">
+      <h1 className=" text-cyan-400 pt-10">Home</h1>
+      <div className=" bg-yellow-500 w-20 h-20 m-auto border-8 border-solid border-blue-700">
+        <p>Hola</p>
       </div>
-    );
-  }
-  
-  export default Home;
-  
+      <div className=" bg-white w-20 h-20 m-auto">
+        <p>Chau</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
